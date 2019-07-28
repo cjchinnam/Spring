75a77a1ed6ea007446fa58dbe1b68f8784a64574
cjchinnam <33137677+cjchinnam@users.noreply.github.com>
2019-07-27 18:57:31 -0700

    Add files via upload

---
+++ spring-testing/src/com/chaitanya/HelloWorld.java
@@ -0,0 +1,5 @@
+package com.jagan;
+
+public class HelloWorld {
+
+}
---
+++ spring-testing/src/com/chaitanya/MainApp.java
@@ -0,0 +1,11 @@
+package com.jagan;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+	}
+
+}
